1917d747d6a05c9ce014bdbc348a24ee446c54a5
CharlieBlain <charlie.blain@hpe.com>
2021-07-07 14:33:24 +0100

    Testing windows install script on github

--- tools/install-hpegl-provider-windows.ps1
+++ tools/install-hpegl-provider-windows.ps1
@@ -1,23 +1,29 @@
+param ($VERSION)
+
 $os="windows"
 $arch="amd64"
 $repo="HewlettPackard/terraform-provider-hpegl"
 $windows_hpegl_dir="$env:appdata\terraform.d\plugins\registry.terraform.io\hewlettpackard\hpegl"
 
-Write-Host Getting latest release
-$release_url="https://api.github.com/repos/${repo}/releases/latest"
-$tag = (Invoke-WebRequest $release_url | ConvertFrom-Json)[0].tag_name
+function get_latest_release {
+    Write-Host Getting latest release
+    $release_url="https://api.github.com/repos/${repo}/releases/latest"
+    $tag = (Invoke-WebRequest $release_url | ConvertFrom-Json)[0].tag_name
+    $VERSION=${tag}
+    
+    $VERSION
+}
 
-Write-Host $tag
+if (!$VERSION) {
+    $VERSION=get_latest_release
+}
 
-$VERSION=${tag}
-$version_number=$VERSION -replace 'v'
+$version_number=$VERSION -replace 'v'  
 
 $dest_dir="${windows_hpegl_dir}\${version_number}\${os}_${arch}\"
 $hpegl_zip="terraform-provider-hpegl_${version_number}_${os}_${arch}.zip"
 $hpegl=$hpegl_zip -replace '.zip'
 $hpegl_dl_url="https://github.com/${repo}/releases/download/${VERSION}/${hpegl_zip}"
-
-Write-Host $hpegl
 
 Write-Host Downloading latest release
 mkdir "$dest_dir" 
@@ -32,4 +38,3 @@ Get-ChildItem -Path $hpegl -Recurse -File | Move-Item -Destination $dest_dir
 Remove-Item $hpegl_zip -Recurse -Force -ErrorAction SilentlyContinue 
 Remove-Item $hpegl -Recurse -Force -ErrorAction SilentlyContinue 
 Write-Host Complete
-
